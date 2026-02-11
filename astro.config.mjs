1922eb961c64d489a77cb26bcbc47345387a8210
pxxguin <janghongseo@naver.com>
2026-02-11 23:05:55 +0900

    chore: Remove `base` configuration from Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pomocal.github.io',
-  base: '/pomocal_web',
   i18n: {
     defaultLocale: 'ko',
     locales: ['ko', 'en'],
