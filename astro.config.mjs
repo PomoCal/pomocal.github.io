ca94b80de60598286b68b75765c1bc69b07fd9dd
pxxguin <janghongseo@naver.com>
2026-02-11 22:50:05 +0900

    feat: Add site and base configuration to Astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pomocal.github.io',
+  base: '/pomocal_web',
   i18n: {
     defaultLocale: 'ko',
     locales: ['ko', 'en'],
